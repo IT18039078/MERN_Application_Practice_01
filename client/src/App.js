36c159702c7fbe7bbd58d01a153f56fe0c510727
IT18039078 <IT18039078@my.sliit.lk>
2022-07-17 13:18:17 +0530

    Delete and added the neccessary file for the front end development

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Preloader from "./component/preloader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <div className="row">
+        <form className="col s12">
+          <div className="row">
+            {/* title  */}
+            <div className="input-field col s6">
+              <i className="material-icons prefix">title</i>
+              <input id="icon_prefix" type="text" className="validate" />
+              <label htmlFor="icon_prefix">Title</label>
+            </div>
+            {/* content */}
+            <div className="input-field col s6">
+              <i className="material-icons prefix">description</i>
+              <input id="icon_telephone" type="tel" className="validate" />
+              <label htmlFor="icon_telephone">Content</label>
+            </div>
+
+          </div>
+        </form>
+        <Preloader/>
+        {/* simple collection that hold the informatio about the todo */}
+        <div class="collection">
+            <a href="#!" class="collection-item">Alvin</a>
+            <a href="#!" class="collection-item active">Alvin</a>
+            <a href="#!" class="collection-item">Alvin</a>
+            <a href="#!" class="collection-item">Alvin</a>
+        </div>
+      </div>
     </div>
   );
 }
