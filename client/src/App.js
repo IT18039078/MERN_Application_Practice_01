1476191229e68e5aec74e3654930cf9edb62bfc6
IT18039078 <IT18039078@my.sliit.lk>
2022-07-18 17:35:30 +0530

    Added the functionality to enter details and selet details for updat 2. also whent the user entered the details the page wil automatically update the system

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,15 @@ function App() {
 
   // todos state for printing the details in list
   const [todos, setTodos] = useState(null) 
+
+  // Get the current id of item printed
+  const [currentId, setCurrentId] = useState(0)
   
+  useEffect(()=>{
+      let currentTodo = currentId != 0 ? todos.find(todo=>todo._id === currentId):{title: '', content:''}
+      setTodo(currentTodo)
+  },[currentId])
+
   useEffect(()=>{
     const fetchData = async()=>{
       const result = await readTodos();
@@ -20,29 +28,45 @@ function App() {
     fetchData()
   }, [])
 
+
+  const clear = () =>{
+    setCurrentId(0);
+    setTodo({title:'', content:''});
+  }
+
+  useEffect(()=>{
+    const clearField = (e) =>{
+      if(e.keyCode === 27){
+        clear()
+      }
+    }
+    window.addEventListener('keydown', clearField)
+    return ()=> window.removeEventListener('keydown', clearField)
+  },[])
   // function to handle the form - onsumbit 
   const onSubmitHandler =  async(e)=>{
       e.preventDefault();
 
       const result  = await createTodo(todo)
-      setTodo(result)    
+      setTodos([...todos,result])    
   }
   
   return (
     <div className="container">
        <div className="row">
-        {/* 
+{/*         
         This preview section for showing the form details in the front to confirm that 
-        we getting the form data when chaning it
+        we getting the form data when chaning it */}
         <pre>
           {JSON.stringify(todo)}
-        </pre> */}
+        </pre>
         <form className="col s12" onSubmit={onSubmitHandler}>
           <div className="row">
             {/* title  */}
             <div className="input-field col s6">
               <i className="material-icons prefix">title</i>
               <input id="icon_prefix" type="text" className="validate" 
+              value={todo.title}
                 onChange={e=>setTodo({...todo, title:e.target.value})}
               />
               <label htmlFor="icon_prefix">Title</label>
@@ -51,6 +75,7 @@ function App() {
             <div className="input-field col s6">
               <i className="material-icons prefix">description</i>
               <input id="icon_telephone" type="tel" className="validate" 
+              value={todo.content}
                 onChange={e=>setTodo({...todo, content:e.target.value})}
               />
               <label htmlFor="icon_telephone">Content</label>
@@ -66,7 +91,8 @@ function App() {
         {
           !todos ? <Preloader/> : todos.length > 0 ? <div className="collection">
             {todos.map((todo)=>(
-              <li key = {todo._id} className="collection-item" style={{listStyle:'none'}}>
+              <li key = {todo._id} onClick={()=>setCurrentId(todo._id)}
+              className="collection-item" style={{listStyle:'none'}}>
                 <div><h5>{todo.title}</h5>
                 <p> {todo.content}
                 <a href="#!" className="secondary-content">
